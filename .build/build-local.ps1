be08c10526402fe6d33fc33a38ced3c3864e180c
Justin Grah <justin@grah.one>
2022-10-14 21:57:21 +0200

    updated local buildscript

--- .build/build-local.ps1
+++ .build/build-local.ps1
@@ -1,8 +1,17 @@
 $containerName = "homepage:devbuild"
 
+# Creating the docker image with the name "hompage:devbuild"
 Write-Host "Creating docker image..."
-docker docker build -t $containerName
+try {
+    docker docker build -t $containerName
+} catch {
+    Write-Error "Error in building docker container"
+    exit
+}
 
-
+# Launching the docker container
 Write-Host "Launching docker container..."
-docker run docker run -d --publish 80:3000 $containerName
+try {
+    docker run docker run -d --publish 80:3000 $containerName
+}
+
